66ee817e77e5bd8c73493708338a104c2b6f4206
linus <linus.bauer@stud.uni-heidelberg.de>
2024-09-18 09:10:42 +0200

    Course Information step 3

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -36,11 +36,21 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const parts = [
-    { part: 'Fundamentals of React', exercises: 10 },
-    { part: 'Using props to pass data', exercises: 7 },
-    { part: 'State of a component', exercises: 14 },
+    part1, part2, part3
   ]
 
   return (
