255c141175205ef5e61b62c0603bce3c51b0a4d0
linus <linus.bauer@stud.uni-heidelberg.de>
2024-09-18 09:12:04 +0200

    Course Information step 3 fixed

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -10,7 +10,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.name} {props.exercises}
       </p>
     </div>
   )
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0].part} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].part} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].part} exercises={props.parts[2].exercises}/>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
   )
 }
