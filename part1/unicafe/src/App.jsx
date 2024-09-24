a0d35fc918d08646dac7b971fe0920e7a53eea80
linus <linus.bauer@stud.uni-heidelberg.de>
2024-09-24 04:22:17 +0200

    <tbody> added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -31,13 +31,15 @@ const Statistics = (props) => {
   return (
     <div>
       <table>
-        <StatisticLine text="good" value={props.good} />
-        <StatisticLine text="neutral" value={props.neutral} />
-        <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
-        <StatisticLine text="bad" value={props.bad} />
-        <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad) * 100} />
-        <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad) * 100} />
+          <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} />
+        </tbody>
       </table>
     </div>)
 }
