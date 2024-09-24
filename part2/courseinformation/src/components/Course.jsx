29560b1155d67083d48d1937f0a34a5e6a12f0bb
linus <linus.bauer@stud.uni-heidelberg.de>
2024-09-24 17:19:44 +0200

    Course information step 7

--- part2/courseinformation/src/components/Course.jsx
+++ part2/courseinformation/src/components/Course.jsx
@@ -1,8 +1,10 @@
 const Course = (props) => {
+    
     return (
         <div>
             <h1>{props.course.name}</h1>
             {props.course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+            <p><b>total of {props.course.parts.reduce((accum,item) => accum + item.exercises, 0)} exercises</b></p>
         </div>
     )
 }
