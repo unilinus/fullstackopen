20156a154b34bc85f72d3cf5175ce445ebb7f7dc
linus <linus.bauer@stud.uni-heidelberg.de>
2024-10-03 06:29:39 +0200

    Phonebook backend step 6

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -28,46 +28,50 @@ let persons = [
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
+})
 
-  app.get('/info', (request, response) => {
+app.get('/info', (request, response) => {
     const currentDate = new Date();
     response.send(`<div><p>Phonebook has info for ${currentDate} people</p><p>${currentDate}</p></div>`)
-  })
+})
 
-  app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(person => person.id === id)
     if (person) {
-      response.json(person)
+        response.json(person)
     } else {
-      response.status(404).end()
+        response.status(404).end()
     }
-  })
+})
 
-  app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
-  })
+})
 
-  const generateId = () => {
+const generateId = () => {
     const rand = Math.floor(Math.random() * 100000000);
-    while(persons.map(p => Number(p.id)).find(n => n === rand)){
+    while (persons.map(p => Number(p.id)).find(n => n === rand)) {
         rand = Math.floor(Math.random() * 100000000);
     }
     return String(rand)
-  }
+}
 
-  app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response) => {
     const body = request.body
-  
+
     if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    } else if (persons.map(p => p.name).find(name => name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
-  
+
     const person = {
         name: body.name,
         number: body.number,
@@ -75,12 +79,12 @@ app.get('/api/persons', (request, response) => {
     }
 
     console.log(person)
-  
+
     persons = persons.concat(person)
     response.json(person)
-  })
+})
 
-  const PORT = 3001
-  app.listen(PORT, () => {
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
