c863c8dfebdead30686f137b2da062e077aa3416
linus <linus.bauer@stud.uni-heidelberg.de>
2024-10-03 06:13:10 +0200

    Phonebook backend step 3

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -35,6 +35,15 @@ app.get('/api/persons', (request, response) => {
     response.send(`<div><p>Phonebook has info for ${currentDate} people</p><p>${currentDate}</p></div>`)
   })
 
+  app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
 
   const PORT = 3001
   app.listen(PORT, () => {
